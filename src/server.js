72fc32cf249d06d1a22300f0283e651075cef728
heart <andrefelipehf2001@gmail.com>
2025-08-25 19:47:45 -0300

    First connected in WebSocket.
    Music: Midnight Pretender

---
+++ src/server.js
@@ -0,0 +1,48 @@
+import url from "url";
+import path from "path";
+import { createServer } from "http";
+
+import express from "express";
+
+// Importar a biblioteca Socket.IO
+import { Server } from "socket.io";
+
+/*
+Socket.io é composta de duas partes:
+
++ Server
++ E uma biblioteca para o cliente que carrega no navegador 
+
+*/
+
+const app = express();
+const server = createServer(app);
+
+const port = process.env.port || 3_005;
+
+// Caminho absoluto do computador
+const atualPath = url.fileURLToPath(import.meta.url);
+const publicDirector = path.join(atualPath, "../..", "public");
+
+// Oferece os arquivos de forma estatica
+app.use(express.static(publicDirector));
+
+/*
+Soket é uma boa solução para problemas que envolvem comunicação
+bidirecional em tempo real entre o cliente e o servidor
+*/
+
+server.listen(
+    port,
+    () => console.log(`Server listening in port ${port}`)
+);
+
+const io = new Server(server);
+/*
+Inicializa uma nova instancia do sokcet.io passando server vomo objeto
+*/
+io.on('connection', (socket) => {
+    console.log('A user connected');
+});
+
+
